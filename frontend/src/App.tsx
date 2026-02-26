c6cc672f23bb0f21c22195629ef0843578d1c021
Bikram Sahu <bikramiter@gmail.com>
2026-02-26 15:28:14 +0530

    chore(frontend): api connection init and check

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,14 @@
+import { useEffect } from "react";
+import api from "./api/client";
+
 function App() {
-  return (
-    <div>
-      <h1>Expense Tracker</h1>
-    </div>
-  );
+  useEffect(() => {
+    api.get("/api/auth/login").catch(() => {
+      console.log("Backend reachable");
+    });
+  }, []);
+
+  return <h1>Expense Tracker</h1>;
 }
 
 export default App;
